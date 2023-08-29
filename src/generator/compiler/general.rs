bd7519ab9b08d76065c74f9ab27221afd7ad3142
Travers Biddle <traversbiddle@gmail.com>
2023-08-29 18:56:31 +1200

    refactor: Implement cstd/cppstd as compiler property

--- src/generator/compiler/general.rs
+++ src/generator/compiler/general.rs
@@ -10,6 +10,25 @@ impl Compiler for GeneralCompiler {
 	}
 
 	fn out_flag(&self) -> String {
-		return "-o".to_owned();
+		"-o".to_owned()
+	}
+
+	fn c_std_flag(&self, std: &str) -> Result<String, String> {
+		match std {
+			"11" => Ok("-std=c11".to_owned()),
+			"17" => Ok("-std=c17".to_owned()),
+			_ => Err(format!("C++ standard not supported by compiler: {std}")),
+		}
+	}
+
+	fn cpp_std_flag(&self, std: &str) -> Result<String, String> {
+		match std {
+			"11" => Ok("-std=c++11".to_owned()),
+			"14" => Ok("-std=c++14".to_owned()),
+			"17" => Ok("-std=c++17".to_owned()),
+			"20" => Ok("-std=c++20".to_owned()),
+			"23" => Ok("-std=c++23".to_owned()),
+			_ => Err(format!("C++ standard not supported by compiler: {std}")),
+		}
 	}
 }
