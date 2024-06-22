34d89ce1f41ba58ace2f3854f718f397b978f925
Travers Biddle <traversbiddle@gmail.com>
2024-06-22 16:45:40 +1200

    feature: Add emscripten as compiler

---
+++ src/toolchain/compiler/emscripten.rs
@@ -0,0 +1,66 @@
+use super::{Compiler, ExeLinker};
+
+pub(crate) struct Emscripten {
+	pub(super) cmd: Vec<String>,
+	pub(super) version: String,
+	pub(super) target: String,
+}
+
+impl Compiler for Emscripten {
+	fn id(&self) -> String {
+		"emscripten".to_owned()
+	}
+
+	fn version(&self) -> String {
+		self.version.clone()
+	}
+
+	fn target(&self) -> String {
+		self.target.clone()
+	}
+
+	fn cmd(&self) -> Vec<String> {
+		self.cmd.clone()
+	}
+
+	fn out_flag(&self) -> String {
+		"-o".to_owned()
+	}
+
+	fn c_std_flag(&self, std: &str) -> Result<String, String> {
+		match std {
+			"11" => Ok("-std=c11".to_owned()),
+			"17" => Ok("-std=c17".to_owned()),
+			_ => Err(format!("C standard not supported by compiler: {std}")),
+		}
+	}
+
+	fn cpp_std_flag(&self, std: &str) -> Result<String, String> {
+		match std {
+			"11" => Ok("-std=c++11".to_owned()),
+			"14" => Ok("-std=c++14".to_owned()),
+			"17" => Ok("-std=c++17".to_owned()),
+			"20" => Ok("-std=c++20".to_owned()),
+			"23" => Ok("-std=c++23".to_owned()),
+			_ => Err(format!("C++ standard not supported by compiler: {std}")),
+		}
+	}
+
+	fn position_independent_code_flag(&self) -> Option<String> {
+		None
+	}
+
+	fn position_independent_executable_flag(&self) -> Option<String> {
+		None
+	}
+}
+
+impl ExeLinker for Emscripten {
+	fn cmd(&self) -> Vec<String> {
+		self.cmd.clone()
+	}
+
+	fn position_independent_executable_flag(&self) -> Option<String> {
+		None
+	}
+}
