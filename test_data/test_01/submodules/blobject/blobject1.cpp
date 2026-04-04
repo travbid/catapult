431a2f05bd9851d09189358b1511d4695c2a8aa4
Travers Biddle <traversbiddle@gmail.com>
2026-04-04 21:45:29 +1100

    test: Add test data to confirm MSVC compiles *.c files as C

--- test_data/test_01/submodules/blobject/blobject1.cpp
+++ test_data/test_01/submodules/blobject/blobject1.cpp
@@ -1,4 +1,4 @@
-#include "blobject.hpp"
+#include "blobject.h"
 
 #include <iostream>
 
---
+++ test_data/test_01/submodules/blobject/blobject2.c
@@ -0,0 +1,11 @@
+#include "blobject.h"
+
+#include <stdio.h>
+
+#if defined(__cplusplus)
+#error "This file should be compiled as C, not C++."
+#endif
+
+void blobject_DoBlob2() {
+    printf("DoBlob2()\n");
+}
